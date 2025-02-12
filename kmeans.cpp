73d0167355540f7d0a875a148c381cd2992b7605
lomi02 <1lomi122702@gmail.com>
2025-02-12 18:21:32 +0100

    Edit kmeans

--- kmeans.cpp
+++ kmeans.cpp
@@ -2,93 +2,85 @@
 using namespace std;
 using namespace cv;
 
-void kmeans(Mat &src, Mat &dst, int k) {
-    srand(time(nullptr));
+Mat kmeans(Mat &input, int k, int maxIterations, double convergenceThreshold) {
+    Mat img = input.clone();
 
     // 1. Inizializzazione casuale dei centroidi
-    vector<uchar> centroids(k);
+    srand(time(nullptr) + 1);
+    vector<uchar> centersIntensity(k);
     for (int i = 0; i < k; i++) {
-        int randRow = rand() % src.rows;
-        int randCol = rand() % src.cols;
-        centroids[i] = src.at<uchar>(randRow, randCol);
+        int x = rand() % img.rows;
+        int y = rand() % img.cols;
+        centersIntensity.at(i) = img.at<uchar>(x, y);
     }
 
-    // 2. Parametri di convergenza
-    double convergenceThreshold = 0.01;
-    int maxIterations = 50;
-    vector<double> oldCentroids(k);
-    vector<double> newCentroids(k);
+    // 2. Parametri
+    int iterations = 0;
+    int closestClusterIndex = 0;
+    bool isCentreUpdated = true;
     vector<vector<Point> > clusters(k);
-    bool isCentroidsChanged = true;
-    int iteration = 0;
+    Mat clusteredImg = img.clone();
 
-    while (isCentroidsChanged && iteration++ < maxIterations) {
-        isCentroidsChanged = false;
-
-        // 3. Pulizia dei cluster
+    // 3. Inizio del ciclo iterativo K-means
+    while (isCentreUpdated || iterations < maxIterations) {
+        isCentreUpdated = false;
         for (int i = 0; i < k; i++)
-            clusters[i].clear();
+            clusters.at(i).clear();
 
-        // 4. Salvataggio dello stato precedente dei centroidi
-        for (int i = 0; i < k; i++)
-            oldCentroids[i] = newCentroids[i];
+        // 4. Assegnazione di ogni pixel al cluster più vicino
+        for (int x = 0; x < img.rows; x++) {
+            for (int y = 0; y < img.cols; y++) {
+                uchar pixelIntensity = img.at<uchar>(x, y);
+                int minDistance = INFINITY;
 
-        // 5. Assegnazione di ciascun pixel al centroide più vicino
-        for (int x = 0; x < src.rows; x++)
-            for (int y = 0; y < src.cols; y++) {
-                uchar pixelValue = src.at<uchar>(x, y);
-                int bestCluster = 0;
-                int minDistance = abs(pixelValue - centroids[0]);
-
-                for (int i = 1; i < k; i++) {
-                    int distance = abs(pixelValue - centroids[i]);
-                    if (distance < minDistance) {
-                        minDistance = distance;
-                        bestCluster = i;
+                for (int i = 0; i < k; ++i) {
+                    int currDistance = abs(centersIntensity.at(i) - pixelIntensity);
+                    if (currDistance < minDistance) {
+                        minDistance = currDistance;
+                        closestClusterIndex = i;
                     }
                 }
-                clusters[bestCluster].push_back(Point(x, y));
+
+                // 5. Aggiunta del punto al cluster più vicino
+                clusters.at(closestClusterIndex).emplace_back(y, x);
+                clusteredImg.at<uchar>(x, y) = centersIntensity.at(closestClusterIndex);
             }
+        }
 
         // 6. Aggiornamento dei centroidi
         for (int i = 0; i < k; i++) {
-            int clusterSize = clusters[i].size();
-            if (clusterSize > 0) {
-                double sum = 0.0;
-                for (int j = 0; j < clusterSize; j++)
-                    sum += src.at<uchar>(clusters[i][j].x, clusters[i][j].y);
-                newCentroids[i] = sum / clusterSize;
-            } else
-                newCentroids[i] = centroids[i];
+            if (!clusters.at(i).empty()) {
+                int intensitySum = 0;
+                for (auto &point: clusters.at(i))
+                    intensitySum += img.at<uchar>(point);
+
+                int newCenter = intensitySum / clusters.at(i).size();
+                int change = abs(newCenter - centersIntensity.at(i));
+
+                // 8. Controllo della convergenza
+                if (change > convergenceThreshold) {
+                    centersIntensity.at(i) = newCenter;
+                    isCentreUpdated = true;
+                }
+            }
         }
-
-        // 7. Controllo della convergenza
-        for (int i = 0; i < k; i++) {
-            if (abs(newCentroids[i] - oldCentroids[i]) > convergenceThreshold)
-                isCentroidsChanged = true;
-            centroids[i] = static_cast<uchar>(newCentroids[i]);
-        }
-
-        cout << "Iteration: " << iteration << endl;
+        iterations++;
     }
-
-    // 8. Creazione dell'immagine segmentata
-    dst = src.clone();
-    for (int i = 0; i < k; i++) {
-        int clusterSize = clusters[i].size();
-        for (int j = 0; j < clusterSize; j++)
-            dst.at<uchar>(clusters[i][j].x, clusters[i][j].y) = centroids[i];
-    }
+    return clusteredImg;
 }
 
 int main(int argc, char **argv) {
     const char *path = argc > 1 ? argv[1] : "../immagini/splash.png";
-    Mat dst, src = imread(samples::findFile(path), IMREAD_GRAYSCALE);
+    Mat src = imread(samples::findFile(path), IMREAD_GRAYSCALE);
 
     //Mat src = imread(argv[1],IMREAD_GRAYSCALE);
     if (src.empty()) return -1;
 
-    kmeans(src, dst, 3);
+    int k = 3;
+    int maxIterations = 30;
+    double deltaTH = 1.0;
+
+    Mat dst = kmeans(src, k, maxIterations, deltaTH);
 
     imshow("K-means", dst);
     waitKey(0);
