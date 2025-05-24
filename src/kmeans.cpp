37d12b2121e0afb36952dde8e616de8a3cd637dd
lomi02 <1lomi122702@gmail.com>
2025-05-24 18:52:05 +0200

    Finalizzazione kmeans

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -2,51 +2,52 @@
 using namespace std;
 using namespace cv;
 
-Mat kmeans_gray(Mat &input, int numberOfClusters, int maxIterations, double deltaTH = 1.0) {
+Mat kmeans_gray(Mat &input, int numberOfCluster, int maxIterations, double deltaTH) {
     Mat img = input.clone();
 
     srand(time(nullptr) + 1);
-    vector<uchar> centres(numberOfClusters);
-    for (int i = 0; i < numberOfClusters; ++i) {
-        int x = rand() % img.cols;
-        int y = rand() % img.rows;
-        centres.at(i) = img.at<uchar>(Point(x, y));
+    vector<uchar> centres(numberOfCluster);
+    for (int i = 0; i < numberOfCluster; i++) {
+        int x = rand() % img.rows;
+        int y = rand() % img.cols;
+
+        centres.at(i) = img.at<uchar>(x, y);
     }
 
     int iterations = 0;
     int closestIndex = 0;
     bool isCentreUpdated = true;
 
-    vector<vector<Point> > clusters(numberOfClusters);
-    Mat clusteredImg = img.clone();
-
-    while (isCentreUpdated || iterations < maxIterations) {
+    vector<vector<Point> > clusters(numberOfCluster);
+    Mat out = input.clone();
+    while (isCentreUpdated && iterations < maxIterations) {
         isCentreUpdated = false;
-        for (int i = 0; i < numberOfClusters; ++i)
+        for (int i = 0; i < numberOfCluster; i++)
             clusters.at(i).clear();
 
-        for (int y = 0; y < img.rows; ++y)
-            for (int x = 0; x < img.cols; ++x) {
-                int minDistance = INFINITY;
-                for (int i = 0; i < numberOfClusters; ++i) {
-                    int currentDistance = abs(centres.at(i) - img.at<uchar>(Point(x, y)));
+        for (int x = 0; x < img.rows; x++)
+            for (int y = 0; y < img.cols; y++) {
+                int minDistance = INT_MAX;
+                for (int i = 0; i < numberOfCluster; i++) {
+                    int currentDistance = abs(centres.at(i) - img.at<uchar>(x, y));
                     if (currentDistance < minDistance) {
                         minDistance = currentDistance;
                         closestIndex = i;
                     }
                 }
                 clusters.at(closestIndex).emplace_back(x, y);
-                clusteredImg.at<uchar>(Point(x, y)) = centres.at(closestIndex);
+                out.at<uchar>(x, y) = centres.at(closestIndex);
             }
 
-        for (int i = 0; i < numberOfClusters; ++i)
+        for (int i = 0; i < numberOfCluster; i++)
             if (!clusters.at(i).empty()) {
                 int intensitySum = 0;
+
                 for (auto &point: clusters.at(i))
                     intensitySum += img.at<uchar>(point);
 
                 double currentMean = intensitySum / clusters.at(i).size();
-                int delta = cvRound(std::abs(currentMean - centres.at(i)));
+                int delta = cvRound(abs(currentMean - centres.at(i)));
 
                 if (delta > deltaTH) {
                     centres.at(i) = cvRound(currentMean);
@@ -55,7 +56,8 @@ Mat kmeans_gray(Mat &input, int numberOfClusters, int maxIterations, double delt
             }
         iterations++;
     }
-    return clusteredImg;
+
+    return out;
 }
 
 int main(int argc, char **argv) {
@@ -67,7 +69,7 @@ int main(int argc, char **argv) {
 
     int k = 3;
     int maxIterations = 30;
-    double deltaTH = 1.0;
+    double deltaTH = 1;
 
     Mat dst = kmeans_gray(src, k, maxIterations, deltaTH);
 
--- srcComment/kmeansComment.cpp
+++ srcComment/kmeansComment.cpp
@@ -6,25 +6,24 @@ using namespace cv;
  * Applica l'algoritmo di clustering k-means a un'immagine in scala di grigi.
  *
  * @param input             Immagine in scala di grigi in input da clusterizzare.
- * @param numberOfClusters  Numero di cluster da creare.
+ * @param numberOfCluster   Numero di cluster da creare.
  * @param maxIterations     Numero massimo di iterazioni per l'algoritmo.
- * @param deltaTH          Soglia per l'aggiornamento dei centri per fermare le iterazioni.
+ * @param deltaTH           Soglia per l'aggiornamento dei centri per fermare le iterazioni.
  * @return                  Immagine clusterizzata in scala di grigi.
  */
-Mat kmeans_gray(Mat &input, int numberOfClusters, int maxIterations, double deltaTH = 1.0) {
+Mat kmeans_gray(Mat &input, int numberOfCluster, int maxIterations, double deltaTH) {
     Mat img = input.clone();
 
     // Passo 1: Inizializza i centri dei cluster in modo casuale.
     srand(time(nullptr) + 1);
-    vector<uchar> centres(numberOfClusters);
-    for (int i = 0; i < numberOfClusters; ++i) {
-
+    vector<uchar> centres(numberOfCluster);
+    for (int i = 0; i < numberOfCluster; i++) {
         // Seleziona una posizione casuale nell'immagine
-        int x = rand() % img.cols;
-        int y = rand() % img.rows;
+        int x = rand() % img.rows;
+        int y = rand() % img.cols;
 
         // Assegna il valore di intensità del pixel casuale come centro del cluster
-        centres.at(i) = img.at<uchar>(Point(x, y));
+        centres.at(i) = img.at<uchar>(x, y);
     }
 
     // Variabili per il controllo delle iterazioni
@@ -33,25 +32,24 @@ Mat kmeans_gray(Mat &input, int numberOfClusters, int maxIterations, double delt
     bool isCentreUpdated = true;
 
     // Inizializza i contenitori per i cluster e l'immagine clusterizzata
-    vector<vector<Point> > clusters(numberOfClusters);
-    Mat clusteredImg = img.clone();
+    vector<vector<Point> > clusters(numberOfCluster);
+    Mat out = input.clone();
 
     // Continua le iterazioni finché i centri si aggiornano o finché non si raggiunge maxIterations
-    while (isCentreUpdated or iterations < maxIterations) {
-
+    while (isCentreUpdated && iterations < maxIterations) {
         // Resetta il flag di aggiornamento e i contenitori dei cluster
         isCentreUpdated = false;
-        for (int i = 0; i < numberOfClusters; ++i)
+        for (int i = 0; i < numberOfCluster; i++)
             clusters.at(i).clear();
 
         // Passo 2: Assegna ogni pixel al cluster con il centro più vicino
-        for (int y = 0; y < img.rows; ++y)
-            for (int x = 0; x < img.cols; ++x) {
-                int minDistance = INFINITY;
+        for (int x = 0; x < img.rows; x++)
+            for (int y = 0; y < img.cols; y++) {
+                int minDistance = INT_MAX;
 
                 // Trova il cluster con il centro più vicino al pixel corrente
-                for (int i = 0; i < numberOfClusters; ++i) {
-                    int currentDistance = abs(centres.at(i) - img.at<uchar>(Point(x, y)));
+                for (int i = 0; i < numberOfCluster; i++) {
+                    int currentDistance = abs(centres.at(i) - img.at<uchar>(x, y));
                     if (currentDistance < minDistance) {
                         minDistance = currentDistance;
                         closestIndex = i;
@@ -59,12 +57,12 @@ Mat kmeans_gray(Mat &input, int numberOfClusters, int maxIterations, double delt
                 }
                 // Aggiungi il pixel al cluster selezionato e aggiorna l'immagine clusterizzata
                 clusters.at(closestIndex).emplace_back(x, y);
-                clusteredImg.at<uchar>(Point(x, y)) = centres.at(closestIndex);
+                out.at<uchar>(x, y) = centres.at(closestIndex);
             }
 
         // Passo 3: Calcola i nuovi centri come media dei pixel nei cluster
-        for (int i = 0; i < numberOfClusters; ++i)
-            if (not clusters.at(i).empty()) {
+        for (int i = 0; i < numberOfCluster; i++)
+            if (!clusters.at(i).empty()) {
                 int intensitySum = 0;
 
                 // Calcola la somma delle intensità dei pixel nel cluster
@@ -73,7 +71,7 @@ Mat kmeans_gray(Mat &input, int numberOfClusters, int maxIterations, double delt
 
                 // Calcola la nuova media e la differenza con il centro precedente
                 double currentMean = intensitySum / clusters.at(i).size();
-                int delta = cvRound(std::abs(currentMean - centres.at(i)));
+                int delta = cvRound(abs(currentMean - centres.at(i)));
 
                 // Aggiorna il centro se la differenza supera la soglia
                 if (delta > deltaTH) {
@@ -83,24 +81,6 @@ Mat kmeans_gray(Mat &input, int numberOfClusters, int maxIterations, double delt
             }
         iterations++;
     }
-    return clusteredImg;
-}
 
-int main(int argc, char **argv) {
-    const char *path = argc > 1 ? argv[1] : "../immagini/splash.png";
-    Mat src = imread(samples::findFile(path), IMREAD_GRAYSCALE);
-
-    //Mat src = imread(argv[1],IMREAD_GRAYSCALE);
-    if (src.empty()) return -1;
-
-    int k = 3;
-    int maxIterations = 30;
-    double deltaTH = 1.0;
-
-    Mat dst = kmeans_gray(src, k, maxIterations, deltaTH);
-
-    imshow("K-means", dst);
-    waitKey(0);
-
-    return 0;
+    return out;
 }
